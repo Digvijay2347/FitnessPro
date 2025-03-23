d85ef0b9bdd549e8da4a4a57d396d1522655118f
Digvijay Singh <90893525+Digvijay2347@users.noreply.github.com>
2025-03-23 10:49:11 +0530

    Update Nutrition.jsx

--- src/components/Nutrition.jsx
+++ src/components/Nutrition.jsx
@@ -1,11 +1,11 @@
-// Nutrition.js
+
 import React from 'react';
 
 function Nutrition() {
   return (
     <div>
       <h2>Nutrition Advice</h2>
-      {/* Add nutrition content here */}
+     
     </div>
   );
 }
